6b0b4dafc82f9033965f847fe53f014546da0b9c
Elisa <bilingualedu@gmail.com>
2021-04-21 11:53:23 -0400

    Feature: Add schema associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,27 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
 
-//associations could go here!
+Order.belongsToMany(Product, { through: 'product_orders' });
+Product.belongsToMany(Order, { through: 'product_orders' });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Address.belongsTo(User);
+User.hasMany(Address);
+
+Product.belongsTo(Category);
+Product.belongsToMany(Tag);
+
+Tag.belongsToMany(Product);
+Category.hasMany(Product);
 
 module.exports = {
   db,
   models: {
-    User,
-  },
-}
+    User
+  }
+};
