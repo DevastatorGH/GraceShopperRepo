9398608d7e222648de6e4c4f5d3831e8cc0f88be
inakins <53452799+inakins@users.noreply.github.com>
2021-04-27 12:16:12 -0400

    Feature: Users route secured for admin only access

--- server/api/users.js
+++ server/api/users.js
@@ -3,7 +3,43 @@ const User = require('../db/models/user');
 
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
+const requireToken = async (req, res, next) => {
+  try {
+    let token;
+    if(req.body.headers){
+      token = req.body.headers.authorization;
+    } else {
+      token = req.headers.authorization;
+    }
+    
+    const user = await User.findByToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const admin = async (req, res, next) => {
+  let token;
+  if(req.body.headers) {
+    token = req.body.headers.authorization
+  } else {
+    token = req.headers.authorization
+  }
+  const user = await User.findByToken(token)
+  req.user = user
+  console.log(req.user)
+  if(user.isAdmin) {
+    next()
+  }
+  else {
+    console.log('Not Authorized!')
+    alert('Not Authorized!')
+  }
+}
+
+router.get('/', requireToken, admin, async (req, res, next) => {
   try {
     const users = await User.findAll();
     res.json(users);
